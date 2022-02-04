c2e4b03416ce1df68d5d66ab079c2602f24e352e
Cyn1ch0 <79491635+Cyn1ch0@users.noreply.github.com>
2022-02-04 14:27:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ function checkpword() {
   if (document.getElementById('pword').value != document.getElementById('cnfrm').value) {
     document.getElementsByClassName('error')[5].textContent = '* Your passwords do not match'
   }
-  fields[4].classList.add('invalid');
-  fields[5].classList.add('invalid');
+  fields[5].style.border="2px solid red";
 }
 
 // Check if one of the required fields is empty
@@ -18,7 +17,7 @@ function checkempty() {
   for (i=0; i<fields.length-1; i++) {
     if (fields[i].value == '') {
       errors[i].textContent = '* This field is required';
-      fields[i+1].classList.add('invalid');
+      fields[i].style.border="2px solid red";
     }
   }
 }
@@ -26,9 +25,7 @@ function checkempty() {
 // Reset the views for the webpage
 function reset() {
   for (i=0; i<fields.length; i++) {
-    if (fields[i].classList.contains('invalid')){
-      fields[i].classList.remove('invalid');
-    }
+    fields[i].style.border="2px solid rgb(194, 194, 194)";
   }
   for (i=0; i<errors.length; i++) {
     errors[i].textContent = '';
